8f025d6511f86a89ecb74fab01c6d627482aca26
Tchangang <tchangang.boris@gmail.com>
2018-02-19 04:47:04 +0100

    change file name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const fs = require("fs")
 const path = require("path")
 
-const CHROME_ORIGIN_PATH = path.join(__dirname, './headless_shell')
+const CHROME_ORIGIN_PATH = path.join(__dirname, './headless_chromium')
 exports.CHROME_PATH = CHROME_ORIGIN_PATH
 
 
