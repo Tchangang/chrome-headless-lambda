1be17bd91e45b0b06f14c0d5b8e54a6150f1732b
Tchangang <tchangang.boris@gmail.com>
2018-02-19 04:28:31 +0100

    init chrome-headless-lambda

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const fs = require("fs")
+const path = require("path")
+
+const CHROME_ORIGIN_PATH = path.join(__dirname, './headless_shell')
+exports.CHROME_PATH = CHROME_ORIGIN_PATH
+
+
+
